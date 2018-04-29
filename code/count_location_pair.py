bad2c54bae04c7284ac753bb035ce7c6b7114deb
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-04-29 13:50:45 -0500

    Add files via upload

---
+++ code/count_location_pair.py
@@ -0,0 +1,23 @@
+import math
+import csv
+from mrjob.job import MRJob
+
+class MRlocation(MRJob):
+
+	def mapper(self, _, line):
+
+		row = next(csv.reader([line]))
+		location = (row[7], row[8])
+
+		yield location, 1
+
+	def combiner(self, loc, counts):
+
+		yield loc, sum(counts)
+
+	def reducer(self, loc, counts):
+
+		yield loc, sum(counts)
+
+if __name__ == '__main__':
+	MRlocation.run()
