062d743fcbacd5ba99bc9c5a096c9abc8baa3e83
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-05-02 18:32:32 -0500

    Add files via upload

--- code/weather.py
+++ code/weather.py
@@ -1,10 +1,32 @@
 from WunderWeather import weather
 import arrow
 import csv
+from datetime import date, timedelta
 
 KEY = '534fe8de0c6c00ce'
 LOCATION = 'NY/New York'
 EXTRACTOR = weather.Extract(KEY)
+
+def get_all_dates(start_y, start_m, start_d, end_y, end_m, end_d):
+	'''
+	Input:
+	  e.g. 2017, 1, 1, 2017, 12, 31
+
+	Return: a list
+	'''
+
+	d1 = date(start_y, start_m, start_d)
+	d2 = date(end_y, end_m, end_d)
+	delta = d2 - d1
+	date_list = []
+	for i in range(delta.days + 1):
+		rv = d1 + timedelta(days=i)
+		rv = rv.strftime('%Y%m%d')
+		date_list.append(rv)
+
+	return date_list
+
+
 
 def get_weather(date):
 	'''
@@ -24,12 +46,21 @@ def get_weather(date):
 	for obs in obs_list:
 		hour = obs['date']['hour']
 		minute = obs['date']['min']
-		temp = obs['tempm']
+		visi = obs['visi']
+		rain = obs['rain']
+		snow = obs['snow']
 		conds = obs['conds']
-		data_list = [date, hour, minute, temp, conds]
+		data_list = [date, hour, minute, visi, rain, snow, conds]
 		with open('weather.csv', 'a') as f:
 			wb = csv.writer(f, delimiter = ',')
 			wb.writerow(data_list)
 
 	return 
+
+if __name__ == '__main__':
+	date_list = get_all_dates(2017, 1, 1, 2017, 1, 31)
+	for date in date_list:
+		get_weather(date)
+
+
 
