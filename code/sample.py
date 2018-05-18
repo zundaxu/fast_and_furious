a09b2b90a0c4a43a388344c006b36f5b1dd1862e
ZhengErWei <31546250+ZhengErWei@users.noreply.github.com>
2018-05-18 16:25:09 -0500

    Update sample.py

--- code/sample.py
+++ code/sample.py
@@ -11,28 +11,6 @@ COLUMNS = ['VendorID', 'tpep_pickup_datetime', 'tpep_dropoff_datetime',
            'fare_amount', 'extra', 'mta_tax', 'tip_amount', 'tolls_amount',
            'improvement_surcharge', 'total_amount']
 
-# TRIP_DF = pd.read_csv('../data/yellow_tripdata_2016-01.csv')
-# TRIP_DF = TRIP_DF.dropna(how='any')
-# SAMPLE_DF = TRIP_DF.sample(n=1000)
-# SAMPLE_DF['start_t'] = pd.to_datetime(SAMPLE_DF['tpep_pickup_datetime'], \
-# 									  format = '%Y-%m-%d %H:%M:%S')
-# SAMPLE_DF['end_t'] = pd.to_datetime(SAMPLE_DF['tpep_dropoff_datetime'], \
-# 									format = '%Y-%m-%d %H:%M:%S')
-# SAMPLE_DF['time_diff'] =  SAMPLE_DF['end_t'] - SAMPLE_DF['start_t']
-# SAMPLE_DF['time_diff'] = SAMPLE_DF['time_diff'].apply(lambda x: x.total_seconds()/60）
-# SAMPLE_DF['speed'] = SAMPLE_DF['trip_distance']/SAMPLE_DF['time_diff']
-# SAMPLE_DF.reset_index(inplace=True)
-# SAMPLE_WEATHER = map_weather_trip.get_weather_df(SAMPLE_DF)
-
-# SAMPLE_DF = pd.concat([SAMPLE_DF, SAMPLE_WEATHER], axis = 1)
-# SAMPLE_DF = SAMPLE_DF[['s_cond', 'e_cond', 's_visi', 'e_visi', 'speed']]
-# SAMPLE_DF['s_visi'] = np.ceil(SAMPLE_DF['s_visi'])
-# SAMPLE_DF['e_visi'] = np.ceil(SAMPLE_DF['e_visi'])
-
-# SAMPLE_DF = SAMPLE_DF.dropna(how='any')
-# SAMPLE_DF.to_csv('sample_2016_1.csv', index = False, 
-# 				header=['s_cond', 'e_cond', 's_visi', 'e_visi', 'speed'])
-
 
 def draw_one_sample(filename):
 
